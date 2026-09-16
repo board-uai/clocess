b11df1806943e695c1cd500574feda195968a059
ArtemKucheruk <a.o.kucheruk@gmail.com>
2026-09-17 00:56:03 +0200

    feat(ssh-sec): create table for private keys, encrypt and decrypt funcs

--- backend/utils/crypt.go
+++ backend/utils/crypt.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	"io"
 )
 
 func GenerateSessionId() (string, error) {
@@ -11,4 +14,35 @@ func GenerateSessionId() (string, error) {
 		return "", err
 	}
 	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func encryptMaster(masterKey, plaintext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(masterKey)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func decryptMaster(masterKey, blob []byte) ([]byte, error) {
+	block, err := aes.NewCipher(masterKey)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	n := gcm.NonceSize()
+	nonce, ct := blob[:n], blob[n:]
+	return gcm.Open(nil, nonce, ct, nil)
 }
